8badf314276178d03679dbe7281833277f1a58e0
Venila Priya <priyagetlucky1011@gmail.com>
2024-10-06 21:16:21 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,50 +2,51 @@ import React, { Component } from 'react';
 import '../styles/App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isBallVisible: false,
-      ballPosition: 0,
+    constructor(props) {
+      super(props);
+      this.state = {
+        isBallVisible: false,
+        ballPosition: 0,
+      };
+      this.handleKeyPress = this.handleKeyPress.bind(this); // Bind method here
+    }
+  
+    componentDidMount() {
+      document.addEventListener('keydown', this.handleKeyPress);
+    }
+  
+    componentWillUnmount() {
+      document.removeEventListener('keydown', this.handleKeyPress);
+    }
+  
+    handleKeyPress(event) {
+      if (event.key === 'ArrowRight' || event.keyCode === 39) {
+        this.setState((prevState) => ({
+          ballPosition: prevState.ballPosition + 5,
+        }));
+      }
+    }
+  
+    buttonClickHandler = () => {
+      this.setState({ isBallVisible: true });
     };
-  }
-
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleKeyPress);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyPress);
-  }
-
-  handleKeyPress = (event) => {
-    if (event.key === 'ArrowRight' || event.keyCode === 39) {
-      this.setState((prevState) => ({
-        ballPosition: prevState.ballPosition + 5,
-      }));
+  
+    renderChoice() {
+      if (this.state.isBallVisible) {
+        return (
+          <div
+            className="ball"
+            style={{ left: `${this.state.ballPosition}px` }}
+          ></div>
+        );
+      } else {
+        return <button className="start" onClick={this.buttonClickHandler}>Start</button>;
+      }
     }
-  };
-
-  buttonClickHandler = () => {
-    this.setState({ isBallVisible: true });
-  };
-
-  renderChoice() {
-    if (this.state.isBallVisible) {
-      return (
-        <div
-          className="ball"
-          style={{ left: `${this.state.ballPosition}px` }}
-        ></div>
-      );
-    } else {
-      return <button className="start" onClick={this.buttonClickHandler}>Start</button>;
+  
+    render() {
+      return <div className="game">{this.renderChoice()}</div>;
     }
   }
-
-  render() {
-    return <div className="game">{this.renderChoice()}</div>;
-  }
-}
-
+  
 export default App;
